331b672ce7a06d330cb77b2ecf942327e6444ea8
masterharoldsaxon4@gmail.com <masterharoldsaxon4@gmail.com>
2021-08-12 23:58:08 +0300

    add func for work with Blocks

--- NewApi.go
+++ NewApi.go
@@ -2,6 +2,7 @@ package docx
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/mallvielfrass/ooxml"
 )
@@ -56,7 +57,23 @@ func (d *Docx) Parser() (Document, error) {
 	}
 	return doc, nil
 }
-func templateBlock() {}
+func templateBlock(s string) WP {
+	r := WP{
+
+		Tag: "w:p",
+		Body: []WPTokens{
+			{
+				Tag:  "w:pPr",
+				Body: `<w:pStyle w:val="Normal"/><w:rPr><w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:eastAsia="Calibri" w:cs="" w:asciiTheme="minorHAnsi" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi"/><w:color w:val="00000A"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr>`,
+			},
+			{
+				Tag:  "w:r",
+				Body: fmt.Sprintf("<w:rPr></w:rPr><w:t>%s</w:t>", s),
+			},
+		},
+	}
+	return r
+}
 func (d *Document) AddNewBlock(s string) {
 	d.WP = append(d.WP, WP{
 		Tag: "w:p",
@@ -72,4 +89,66 @@ func (d *Document) AddNewBlock(s string) {
 			},
 		},
 	})
+}
+func (d *Document) GetBlockIDByTag(s string) (int, error) {
+	for i, item := range d.WP {
+		for _, itemD := range item.Body {
+			// if strings.Contains(itemD.Body s) {
+
+			// 		}
+			if itemD.Tag == "w:r" {
+				if strings.Contains(itemD.Body, s) {
+					return i, nil
+				}
+			}
+		}
+
+	}
+	return 0, fmt.Errorf("block with tag not found")
+}
+func (d *Document) EditBlockByID(id int) {
+
+}
+func (d *Document) ReplaceTagString(oldTag, newString string) error {
+	id, err := d.GetBlockIDByTag(oldTag)
+	if err != nil {
+		//t.Error(err)
+		return err
+	}
+	for i, itemD := range d.WP[id].Body {
+		// if strings.Contains(itemD.Body s) {
+
+		// 		}
+		if itemD.Tag == "w:r" {
+			if strings.Contains(itemD.Body, oldTag) {
+				d.WP[id].Body[i].Body = strings.Replace(itemD.Body, oldTag, newString, -1)
+				return nil
+			}
+		}
+	}
+	return fmt.Errorf("tag not found")
+}
+func (d *Document) GetBlockByID(id int) WP {
+	return d.WP[id]
+}
+
+func (d *Docx) Compile(path string, doc Document) error {
+	//head, _, footer := d.ParseNode()
+	return nil
+}
+func (d *Document) BodyToString() string {
+	var body string
+	for _, item := range d.WP {
+		body += wpTokenToString(item)
+	}
+	body += "<" + d.SectPr.Tag + ">" + d.SectPr.Body + "</" + d.SectPr.Tag + ">"
+	return ""
+}
+func wpTokenToString(item WP) string {
+	//item.Tag
+	var body string
+	for _, it := range item.Body {
+		body += it.Tag + it.Attr + it.Body + it.Tag
+	}
+	return body
 }
--- NewApi_test.go
+++ NewApi_test.go
@@ -1,6 +1,7 @@
 package docx
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -122,5 +123,146 @@ func TestAddNewBlock(t *testing.T) {
 	assert.Equal(t, expected, doc)
 }
 func TestTemplateBlock(t *testing.T) {
+	exp := WP{
+		Tag: "w:p",
+		Body: []WPTokens{
+			{
+				Tag:  "w:pPr",
+				Body: `<w:pStyle w:val="Normal"/><w:rPr><w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:eastAsia="Calibri" w:cs="" w:asciiTheme="minorHAnsi" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi"/><w:color w:val="00000A"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr>`,
+			},
+			{
+				Tag:  "w:r",
+				Body: fmt.Sprintf("<w:rPr></w:rPr><w:t>%s</w:t>", "test"),
+			},
+		},
+	}
+	res := templateBlock("test")
+	assert.Equal(t, exp, res)
+}
+func TestGetBlockByIDName(t *testing.T) {
+	r, err := ReadDocxFile("./testingXML/TestDocumentTag2.docx")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	docx1 := r.Editable()
+	doc, err := docx1.Parser()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	id, err := doc.GetBlockIDByTag("{Name}")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	assert.Equal(t, 0, id)
+}
+func TestGetBlockByIDNameBroken(t *testing.T) {
+	r, err := ReadDocxFile("./testingXML/TestDocumentTag2.docx")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	docx1 := r.Editable()
+	doc, err := docx1.Parser()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = doc.GetBlockIDByTag("{BrokenName}")
+	if err == nil {
+		t.Error(err, "expected error")
+		return
+	}
+	//assert.Equal(t, 0, id)
+}
+func TestEditBlockByID(t *testing.T) {
+	r, err := ReadDocxFile("./testingXML/TestDocumentTag2.docx")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	docx1 := r.Editable()
+	doc, err := docx1.Parser()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	id, err := doc.GetBlockIDByTag("{Name}")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	assert.Equal(t, 0, id)
 
+}
+func TestReplaceTagString(t *testing.T) {
+	r, err := ReadDocxFile("./testingXML/TestDocumentTag2.docx")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	docx1 := r.Editable()
+	doc, err := docx1.Parser()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = doc.ReplaceTagString("{Name}", "KVA")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	//	fmt.Println(doc.WP[0].Body)
+	exp := `<w:rPr><w:rFonts w:eastAsia="Calibri" w:cs="" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi"/><w:color w:val="00000A"/><w:kern w:val="0"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr><w:t>Name: KVA</w:t>`
+	assert.Equal(t, exp, doc.WP[0].Body[len(doc.WP[0].Body)-1].Body)
+}
+func TestCompile(t *testing.T) {
+	r, err := ReadDocxFile("./testingXML/TestDocumentTag2.docx")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	docx1 := r.Editable()
+	doc, err := docx1.Parser()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = doc.ReplaceTagString("{Name}", "KVA")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	//	fmt.Println(doc.WP[0].Body)
+	exp := `<w:rPr><w:rFonts w:eastAsia="Calibri" w:cs="" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi"/><w:color w:val="00000A"/><w:kern w:val="0"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr><w:t>Name: KVA</w:t>`
+	assert.Equal(t, exp, doc.WP[0].Body[len(doc.WP[0].Body)-1].Body)
+	//	expDoc := `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	//<w:document xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" mc:Ignorable="w14 wp14"><w:body><w:p><w:pPr><w:pStyle w:val="Normal"/><w:rPr><w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:eastAsia="Calibri" w:cs="" w:asciiTheme="minorHAnsi" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi"/><w:color w:val="00000A"/><w:kern w:val="0"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr></w:pPr><w:r><w:rPr><w:rFonts w:eastAsia="Calibri" w:cs="" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi"/><w:color w:val="00000A"/><w:kern w:val="0"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr><w:t>Name: {Name}</w:t></w:r></w:p><w:p><w:pPr><w:pStyle w:val="Normal"/><w:rPr><w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:eastAsia="Calibri" w:cs="" w:asciiTheme="minorHAnsi" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi"/><w:color w:val="00000A"/><w:kern w:val="0"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr></w:pPr><w:r><w:rPr><w:rFonts w:eastAsia="Calibri" w:cs="" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi"/><w:color w:val="00000A"/><w:kern w:val="0"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr><w:t>Log: {Log} Date {Date}</w:t></w:r></w:p><w:sectPr><w:headerReference w:type="default" r:id="rId2"/><w:footerReference w:type="default" r:id="rId3"/><w:type w:val="nextPage"/><w:pgSz w:w="12240" w:h="15840"/><w:pgMar w:left="1440" w:right="1440" w:header="720" w:top="1440" w:footer="720" w:bottom="1440" w:gutter="0"/><w:pgNumType w:fmt="decimal"/><w:formProt w:val="false"/><w:textDirection w:val="lrTb"/><w:docGrid w:type="default" w:linePitch="360" w:charSpace="0"/></w:sectPr></w:body></w:document>`
+	err = docx1.Compile("./doc.docx", doc)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	//	assert.Equal(t, expDoc, docx1.content)
+}
+func TestBodyToString(t *testing.T) {
+	r, err := ReadDocxFile("./testingXML/TestDocumentTag.docx")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	docx1 := r.Editable()
+	doc, err := docx1.Parser()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	expected := `<w:p><w:pPr><w:pStyle w:val="Normal"/><w:rPr><w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:eastAsia="Calibri" w:cs="" w:asciiTheme="minorHAnsi" w:cstheme="minorBidi" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi"/><w:color w:val="00000A"/><w:sz w:val="24"/><w:szCs w:val="24"/><w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/></w:rPr></w:pPr><w:r><w:rPr></w:rPr><w:t>This is a</w:t></w:r><w:bookmarkStart w:id="0" w:name="_GoBack"/><w:bookmarkEnd w:id="0"/><w:r><w:rPr></w:rPr><w:t xml:space="preserve"> word document.</w:t></w:r></w:p><w:sectPr><w:headerReference w:type="default" r:id="rId2"/><w:footerReference w:type="default" r:id="rId3"/><w:type w:val="nextPage"/><w:pgSz w:w="12240" w:h="15840"/><w:pgMar w:left="1440" w:right="1440" w:header="720" w:top="1440" w:footer="720" w:bottom="1440" w:gutter="0"/><w:pgNumType w:fmt="decimal"/><w:formProt w:val="false"/><w:textDirection w:val="lrTb"/><w:docGrid w:type="default" w:linePitch="360" w:charSpace="0"/></w:sectPr>`
+	res := doc.BodyToString()
+	assert.Equal(t, expected, res)
+}
+func TestWpTokenToString(t *testing.T) {
+	t.Error(fmt.Errorf("broke test"))
 }
